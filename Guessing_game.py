0d0c618ccde52e6fad114659e3c867161a9a3700
RichGH007 <richard.viloria72@gmail.com>
2021-06-18 22:43:25 -0500

    Update Guessing_game.py
    Met all of the "exceed expectations" grade but still need to account for user error when asked if want to play again.

--- Guessing_game.py
+++ Guessing_game.py
@@ -1,41 +1,7 @@
-# """
-# Python Web Development Techdegree
-# Project 1 - Number Guessing Game
-# --------------------------------
-
-# For this first project we will be using Workspaces.
-
-# NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> #Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-# """
-
-#import random
-
-
-# def start_game():
-#    """Psuedo-code Hints
-#
-#    When the program starts, we want to:
-#    ------------------------------------
-#    1. Display an intro/welcome message to the player.
-#    2. Store a random number as the answer/solution.
-#    3. Continuously prompt the player for a guess.
-#      a. If the guess greater than the solution, display to the player "It's lower".
-#      b. If the guess is less than the solution, display to the player "It's higher".
-#
-#    4. Once the guess is correct, stop looping, inform the user they "Got it"
-#         and show how many attempts it took them to get the correct number.
-#    5. Let the player know the game is ending, or something that indicates the game is over.
-#
-#    ( You can add more features/enhancements if you'd like to. )
-#    """
-#    # write your code inside this function.
-
-# Kick off the program by calling the start_game function.
-# start_game()
 import random
 counter = 0
 new_game = True
+score_list = []
 
 print("------------------------------------------")
 print("-                                        -")
@@ -45,8 +11,17 @@ print("-    if it's too high or too low.  We    -")
 print("-  will tell you how guesses you made.   -")
 print("-                                        -")
 print("------------------------------------------")
+print()
 
 while new_game:
+    if len(score_list) == 0:
+        print("No scores recorded yet.")
+    if len(score_list) > 0:
+        print()
+        print("Playing again...these are the existing scores: ")
+        for score in score_list:
+            print("* " + str(score))
+        print()
     num = random.randint(1, 20)
     # print(num)
     guess_NOTvalid = True
@@ -74,9 +49,22 @@ while new_game:
                 if guessed_num == num:
                     counter += 1
                     guess_NOTvalid = False
-                    print("Random number is {} and you guessed {} times".format(
+                    print("You GOT it! Random number is {} and you guessed {} times".format(
                         num, counter))
-
-        # Ask the player if (s)he wants to try again.
-
+                    score_list.append(counter)
+                    if len(score_list) == 1:
+                        print("Your score of {} is the best score, but it's also the only score.".format(
+                            counter))
+                        print()
+                    if len(score_list) > 1:
+                        print()
+    print("Would you like to play again? (y/n)")
+    play = input("> ")
+    if (play.lower() == "y"):
+        new_game = True
+        counter = 0
+    if (play.lower() == "n"):
         new_game = False
+print("End game.")
+print()
+print()
